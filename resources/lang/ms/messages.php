6bb1fbced0e32535ba41c82ae87269f8d6cd70a6
Fikri Mastor <fikrimastor@gmail.com>
2024-05-22 03:30:01 +0800

    feat: change invalid_state messages #8

--- resources/lang/ms/messages.php
+++ resources/lang/ms/messages.php
@@ -4,5 +4,5 @@ return [
     'invalid_character' => ':attribute mengandungi aksara tidak sah untuk MyKad.',
     'invalid_length' => ':attribute mesti mempunyai 12 aksara.',
     'invalid_birth_date' => ':attribute tidak mengandungi tarikh lahir yang sah.',
-    'invalid_state' => ':attribute tidak mengandungi kod negeri yang sah.',
+    'invalid_state' => ':attribute tidak mempunyai kod negeri yang sah.',
 ];
